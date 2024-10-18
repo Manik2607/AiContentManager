f880260e951c959c42f12b11b513bbf454d69036
JEEVA-M01 <150790709+JEEVA-M01@users.noreply.github.com>
2024-10-18 16:18:21 +0530

    removed unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 
 import './App.css'
 import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
